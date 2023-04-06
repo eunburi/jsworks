061d173badb583794766bc939ffbfb0452185a96
eunburi <finestar23@naver.com>
2023-04-06 18:20:07 +0900

    validation

--- ch07/js/major.js
+++ ch07/js/major.js
@@ -1,10 +1,9 @@
-var selectMenu = document.testForm.major;
+//var selectMenu = document.testForm.major;
 
 function displaySelect(){
-    //select option속성의 인덱스로 선택
-    //var selectedText = selecMenu.options[]
 
-    var selectedText = document.getElementById("major").value;
+    // id 선택자
+    let selectedText = document.getElementById("major").value;
     alert(selectedText + "가 선택되었습니다.");
 
 
--- ch07/js/order.js
+++ ch07/js/order.js
@@ -1,4 +1,5 @@
 //체크박스 속성에 이벤트 처리하기
+//window.onload = function(){} //익명실행함수
 let check = document.querySelector("#shippingInfo");
 
 //체크 이벤트 구현
@@ -16,11 +17,13 @@ let shippingAddr = document.getElementById("shippingAddr");
 
 
 function checkBox(){
+    //체크시 기본정보 값을 배송정보값에 저장
     if(check.checked == true){
         shippingName.value = billingName.value;
         shippingTel.value = billingTel.value;
         shippingAddr.value = billingAddr.value;
     }else{
+        //체크해제시 check.checked == fasle 입력상자 공백초기화
         shippingName.value = "";
         shippingTel.value = "";
         shippingAddr.value = "";
